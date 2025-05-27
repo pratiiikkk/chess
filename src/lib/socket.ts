90bb94cbdc0c9382370285e3ef68bd75064d24a5
pratiiikkk <neexzz05@gmail.com>
2025-05-27 12:45:03 +0530

    init add socket.io

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,11 +1,9 @@
 import { io, Socket } from "socket.io-client";
-import { ClientToServerEvents, ServerToClientEvents } from "@/types/socket";
 
 const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL||"http://localhost:3000";
+;
 
-export type TypedSocket = Socket<ServerToClientEvents, ClientToServerEvents>;
-
-export const createSocket = (): TypedSocket => {
+export const createSocket = () => {
   return io(SOCKET_URL, {
     autoConnect: false,
     reconnection: true,
